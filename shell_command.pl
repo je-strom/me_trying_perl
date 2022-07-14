88ea2ff453e2e91c9b60279dafa0afb917dab707
je-strom <jestrom@protonmail.com>
2022-07-14 22:48:20 +0200

    Execute shell commands.

---
+++ shell_command.pl
@@ -0,0 +1,16 @@
+use warnings;
+use strict;
+use v5.36;
+
+my $command;
+print("type quit to quit :)\n");
+do {
+	print("shell command > ");
+	chomp($command = <STDIN>);
+	if($command eq "quit") {
+		exit 0;
+	}
+	system($command);
+	print("\n");
+}
+while(1);
