a9099e91328f10aa75b5390667b5e5e0469f8bda
je-strom <jestrom@protonmail.com>
2022-07-19 02:05:50 +0200

    now with regex.

--- inch_fraction2_mm.pl
+++ inch_fraction2_mm.pl
@@ -7,6 +7,7 @@ print("Converts Inch fractions to decimal millimeter form\n");
 print("Example input 1 1/3 , 0 1/3\n");
 
 $user_input = <<>>;
-my @tmp  = split(/[" ", "\/"]/ ,   $user_input); 
+#my @tmp  = split(/[" ", "\/"]/ ,   $user_input); 
+my @tmp = $user_input =~ (/(\d+\s)\s*(\d+)\s*\D\s*(\d+)/);
 $result = (($tmp[1] / $tmp[2]) + $tmp[0]) * 25.4 ;
 print("Inch fraction to MM = $result \n");
