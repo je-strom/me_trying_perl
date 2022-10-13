f98d84efa5e3f235d8a0107ef57b634b183ecd0d
je-strom <je-strom@protonmail.com>
2022-10-13 21:07:20 +0200

    new

--- inch_fraction2_mm.pl
+++ inch_fraction2_mm.pl
@@ -7,11 +7,11 @@ use strict;
 use v5.32;
 my ($user_input, ,$result);
 
-print("Converts Inch fractions to decimal millimeter form\n");
-print("Example input 1 1/3 , 0 1/3\n");
+say("Converts Inch fractions to decimal millimeter form\n");
+say("Example input 1 1/3 , 0 1/3\n");
 
 $user_input = <<>>;
 #my @tmp  = split(/[" ", "\/"]/ ,   $user_input); 
 my @tmp = $user_input =~ (/(\d+\s)\s*(\d+)\s*\D\s*(\d+)/);
 $result = (($tmp[1] / $tmp[2]) + $tmp[0]) * 25.4 ;
-print("Inch fraction to MM = $result \n");
+say("Inch fraction to MM = $result \n");
